e151e8199c6e73027a497d824ef6393ca93c979d
Adam Kozlowski <koz.adamxd@gmail.com>
2026-06-14 18:11:17 +0200

    3D lighting: balanced between too bright and too dark

--- components/WatchModel3D.tsx
+++ components/WatchModel3D.tsx
@@ -32,10 +32,10 @@ export default function WatchModel3D() {
       style={{ width: "100%", height: "100%" }}
       gl={{ antialias: true, alpha: true }}
     >
-      <ambientLight intensity={0.2} />
-      <directionalLight position={[5, 5, 5]} intensity={0.5} color="#ffffff" />
-      <directionalLight position={[-4, 2, -2]} intensity={0.15} color="#8888ff" />
-      <pointLight position={[0, 3, 2]} intensity={0.2} color="#ffffff" />
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 5]} intensity={0.9} color="#ffffff" />
+      <directionalLight position={[-4, 2, -2]} intensity={0.2} color="#aaaaff" />
+      <pointLight position={[0, 3, 2]} intensity={0.35} color="#ffffff" />
       <Suspense fallback={null}>
         <WatchMesh />
         <Environment preset="night" />
